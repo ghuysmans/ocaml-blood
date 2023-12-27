1a2e056a1a2f880afe770c27374dbd7d806c821c
Guillaume Huysmans <ghuysmans99@gmail.com>
2023-12-27 17:28:52 +0100

    Materialize compatible couples

--- abor.ml
+++ abor.ml
@@ -5,7 +5,26 @@ module ABO = struct
     | AB : ([`A], [`B], [< `A | `Nil], [< `B | `Nil]) t
     | O : ([`Nil], [`Nil], [< `A | `Nil], [< `B | `Nil]) t
 
-  let compatible (d : ('a, 'b, _, _) t) (r : (_, _, 'a, 'b) t) = ()
+  type c =
+    C : {donor:     ('a, 'b, _, _) t;
+         recipient: (_, _, 'a, 'b) t} -> c
+
+  let compatible =
+    let compatible donor recipient = C {donor; recipient} in
+    [
+      (* universal donor *)
+      compatible O O;
+      compatible O A;
+      compatible O B;
+      (* universal recipient *)
+      compatible O AB;
+      compatible A AB;
+      compatible B AB;
+      (* self-compatibility *)
+      compatible AB AB;
+      compatible A A;
+      compatible B B;
+    ]
 
   let to_string : type a b c d. (a, b, c, d) t -> string = function
     | A -> "A"
@@ -19,7 +38,17 @@ module Rh = struct
     | P : ([`D], [< `D | `Nil]) t
     | N : ([`Nil], [`Nil]) t
 
-  let compatible (d : ('a, _) t) (r : (_, 'a) t) = ()
+  type c =
+    C : {donor:     ('a, _) t;
+         recipient: (_, 'a) t} -> c
+
+  let compatible =
+    let compatible donor recipient = C {donor; recipient} in
+    [
+      compatible P P;
+      compatible N P;
+      compatible N N;
+    ]
 
   let to_string : type d r. (d, r) t -> string = function
     | P -> "+"
@@ -42,41 +71,43 @@ let bn = ABO.B, Rh.N
 let abn = ABO.AB, Rh.N
 let on = ABO.O, Rh.N
 
-let compatible (x, y) (x', y') =
-  ABO.compatible x x';
-  Rh.compatible y y'
+type c =
+  C : {donor:     ('a, 'b, 'd, _, _, _) t;
+       recipient: (_, _, _, 'a, 'b, 'd) t} -> c
 
-
-let () =
-  (* universal donor *)
-  compatible on on;
-  compatible on an;
-  compatible on bn;
-  (* universal recipient *)
-  compatible on abn;
-  compatible an abn;
-  compatible bn abn;
-  compatible abn abn;
-  (* meh *)
-  compatible an an;
-  compatible bn bn;
-  (* same for Rh *)
-  compatible op op;
-  compatible op ap;
-  compatible op bp;
-  compatible op abp;
-  compatible ap ap;
-  compatible ap abp;
-  compatible bp bp;
-  compatible bp abp;
-  compatible abp abp;
-  (* Rh- -> Rh+ *)
-  compatible on op;
-  compatible on ap;
-  compatible on bp;
-  compatible on abp;
-  compatible an ap;
-  compatible an abp;
-  compatible bn bp;
-  compatible bn abp;
-  compatible abn abp;
+let compatible =
+  let compatible donor recipient = C {donor; recipient} in
+  [
+    (* universal donor *)
+    compatible on on;
+    compatible on an;
+    compatible on bn;
+    (* universal recipient *)
+    compatible on abn;
+    compatible an abn;
+    compatible bn abn;
+    compatible abn abn;
+    (* meh *)
+    compatible an an;
+    compatible bn bn;
+    (* same for Rh *)
+    compatible op op;
+    compatible op ap;
+    compatible op bp;
+    compatible op abp;
+    compatible ap ap;
+    compatible ap abp;
+    compatible bp bp;
+    compatible bp abp;
+    compatible abp abp;
+    (* Rh- -> Rh+ *)
+    compatible on op;
+    compatible on ap;
+    compatible on bp;
+    compatible on abp;
+    compatible an ap;
+    compatible an abp;
+    compatible bn bp;
+    compatible bn abp;
+    compatible abn abp;
+  ]
